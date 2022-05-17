84be38c047fdb045d2ef8cf481df691629fc4db3
Gabriel Depaillat <63260459+drace94@users.noreply.github.com>
2022-05-17 23:39:23 +0200

    Add files via upload

--- arch_grid.py
+++ arch_grid.py
@@ -2,7 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from random import sample
 from collections import deque
+import sys
 
+tol = 1e-6
+eps = 5e-2
 # =============================================================================
 # CLASS NETWORK
 # =============================================================================
@@ -36,7 +39,7 @@ class Network:
 # =============================================================================
         
 class Job:
-    def __init__(self, load, wtime, network, run = False, wait = False, end = False):
+    def __init__(self, load, wtime, network, run = False, wait = True, end = False):
         self.counter += 1
         self.network = network
         self.id = self.counter
@@ -55,7 +58,7 @@ class Job:
     def wait(self, dt, network, old_cluster, new_cluster):
         # decrease the waiting time according to the ODE, explicit Euler scheme
         if self.is_running and old_cluster != new_cluster :
-            transfer_time = network.get_transfer_time(self.cluster,new_cluster)
+            transfer_time = network.get_transfer_time(old_cluster,new_cluster)
             self.waiting_time += - self.waiting_time + transfer_time
         else :
             self.waiting_time -= dt
@@ -65,7 +68,7 @@ class Job:
 # =============================================================================
         
 class Cluster:
-    def __init__(self, network, pos, speed, power, N_proc, tau_pr, security_rate = 0.9):
+    def __init__(self, network, pos, speed, power, N_proc, tau_pr, delta = 0, security_rate = 0.9):
         self.network = network
         self.position = pos # position of the cluster within the network
         self.computation_speed = speed
@@ -77,23 +80,27 @@ class Cluster:
         self.theta_star = 0
         self.liste_theta_star = deque()
         self.tau_pr = tau_pr
+        self.delta = delta
         
     def energy_consumption(self, rate):
         # rate : current rate of occupation of the cluster (between 0 and 1)
         # the energy consumption is considered to be piecewise linear here
-        return (rate > 0.5 )*(rate*7 - 3) + (rate <= 0.5 )*rate
+        return (rate > 0.5 )*(rate*7 - 3) + (rate <= 0.5 )*rate + self.delta
         
     def add_to_queue(self, job):
         self.wait_job.append(job)
         job.is_waiting = True
         job.is_running = False
         
+    def get_len(self):
+        return len(self.wait_job)
+        
     def get_occup(self):
         # we consider 1 job per processor
         # the utilization is equal to the number of running jobs
         return len(self.run_job)
         
-    def Decision(self, network, job, energy_coef, time_coef):
+    def Decision(self, network, job):
         # optimizing speed and energy consumption of the network with transfers
         E = network.energy_coef
         T = network.time_coef
@@ -104,19 +111,23 @@ class Cluster:
         for i in range(network.get_number_cluster()):
             # case 1 : the observed cluster is the current cluster of the job
             if i == job.cluster:
-                list1.append(self.energetic_power*self.energy_consumption(self.get_occup() / self.total_proc))
+                # job.load*self.energetic_power*
+                list1.append(self.energy_consumption(min(self.get_occup()+self.get_len(),self.max_proc ) / self.total_proc))
                 list2.append(job.load / self.computation_speed)
             # case 2 : else...
             else :
                 cl = network.clusters[i]
-                list1.append(cl.energetic_power*cl.energy_consumption(cl.get_occup() / cl.total_proc))
+                # job.load*cl.energetic_power*
+                list1.append((1-eps)*cl.energy_consumption(min(cl.get_occup()+cl.get_len(),cl.max_proc ) / cl.total_proc))
                 list2.append(job.load/cl.computation_speed 
                              - cl.theta_star 
                              + network.get_transfer_time(job.cluster,i))
             list3.append(E*list1[i] + T*list2[i])
-        return np.argmin(list3) # pos of the best cluster            
+        # print(list3)
+        return np.argmin(list3) # pos of the best cluster   
     
     def update(self,dt):
+        
         Nb_job_test = int(dt/self.tau_pr)
         # Nb_job_test = 1
         
@@ -126,15 +137,18 @@ class Cluster:
         # availibility of the cluster : max processors - occupied processors
         avail = self.max_proc - self.get_occup()
         # update status of jobs from the queue
+        
         for job in self.wait_job:
-            if job.waiting_time <= self.theta_star and (avail > 0):
+            #if self.position == 1 :
+            #   print(job.waiting_time)
+            if job.waiting_time <= 0 and (avail > 0) :
                 job.is_running = True
                 job.is_waiting = False
+                self.run_job.append(job)
                 avail -= 1
         
-        self.run_job = deque([job for job in self.wait_job if job.is_running] + [job for job in self.run_job])
         self.wait_job = deque([job for job in self.wait_job if job.is_waiting])
-        
+
         for i in range(len(self.wait_job)):
             if self.wait_job[i].waiting_time <= 0 : # not transfering or on waiting
                 waiting_time.append(self.wait_job[i].waiting_time) 
@@ -145,14 +159,15 @@ class Cluster:
         else:
             self.theta_star = 0
             self.liste_theta_star.append(self.theta_star)
-
+            
         sample_for_test = deque(sample(self.run_job,min(Nb_job_test,len(self.run_job))))
+        # sample_for_test = deque(sample(self.run_job,len(self.run_job)))
         other_jobs = deque([job for job in self.run_job if job not in sample_for_test])
         
         ## RUN THE CURRENT JOBS 
         for job in sample_for_test:
             job.run(dt)
-            new_cluster = self.Decision(self.network, job, self.network.energy_coef, self.network.time_coef)
+            new_cluster = self.Decision(self.network, job)
             job.wait(dt, self.network, self.position, new_cluster)
             # proceed the transfer
             if new_cluster != job.cluster :
@@ -161,11 +176,10 @@ class Cluster:
         for job in other_jobs:
             job.run(dt)
             job.wait(dt, self.network, self.position,self.position)
-                
-        # update running jobs list and clear the cluster from empty jobs
-        self.run_job = deque([job for job in self.run_job if job.cluster == self.position and job.load > 0])
+        
+        self.run_job = deque([job for job in self.run_job if job.cluster == self.position and job.load > tol])
         
         ## COMPUTE WAITING TIME FOR JOBS IN THE QUEUE
-        for w in range(len(self.wait_job)):
+        for job in self.wait_job:
             # update the waiting time of waiting jobs
-            self.wait_job[w].wait(dt, self.network, self.position,self.position)
+            job.wait(dt, self.network, self.position,self.position)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from arch_grid import *
+import numpy.linalg as npl
 import time
 # =============================================================================
 # PRE-PROCESSING
@@ -6,13 +7,14 @@ import time
 Job.counter = 0
 plt.close('all')
 # Network init
-c = 1000000000
+c = 100000000000
 A = np.array([[0,c],[c,0]])
 coeff_energy = 0
 coeff_time = 1
 tau_pr = 1e-4 #eps^2
 dt = 1e-2 #eps
 T = 1
+
 
 print("tau_pr = %f"%tau_pr)
 print("dt = %f"%dt)
@@ -31,7 +33,7 @@ if abs(np.log(N_proc/Nb_Jobs)) > np.log(9.9) :
 network1 = Network(A,coeff_energy,coeff_time)
 # Cluster parameters : network | position | speed | power | Nb processors
 cluster1 = Cluster(network1, 0, 50 , 1, N_proc, tau_pr)
-cluster2 = Cluster(network1, 1, 100, 1, N_proc, tau_pr)
+cluster2 = Cluster(network1, 1, 100, 1, N_proc, tau_pr, delta = 1e-3)
 # Add clusters to the current network
 network1.add_cluster(cluster1)
 network1.add_cluster(cluster2)
@@ -39,10 +41,10 @@ network1.add_cluster(cluster2)
 # Jobs init
 n = np.arange(0,Nb_Jobs)
 np.random.seed(2)
-rand_load = np.random.uniform(50,200,len(n))
+rand_load = np.random.uniform(50,150,len(n))
 # rand_load = np.ones(len(n))*100
 rand_wtime = np.zeros(len(n))
-#rand_wtime = np.random.uniform(0,10,len(n))
+# rand_wtime = np.random.uniform(0,10,len(n))
 # Assign jobs
 for i in n:
     job = Job(rand_load[i], rand_wtime[i],  network1)
@@ -53,26 +55,36 @@ for i in n:
 # =============================================================================   
 
 t0 = 0
-tf = 100
+tf = 50
 N = int((tf-t0)/dt)
 tn = np.linspace(t0,tf,N+1)
 
 t1 = time.time()
+nb_job_soumis = 0
 load1 = []
+list1 = []
 load2 = []
-s =  4
+list2 = []
+f = 100
+per = 1/f
+stack = 0
+n = 1
 for t in tn:
-    i=0
-    for i in range(s):
-        network1.assign(cluster1.position, Job(50, 0,  network1))
-        
-    #for j in range(s):
-        #network1.assign(cluster2.position, Job(200, 0,  network1))
+    if np.isclose(stack, per) :
+        if stack >= per :
+            for i in range(n):
+                network1.assign(cluster1.position, Job(225, 0,  network1))
+                network1.assign(cluster2.position, Job(225, 0,  network1))
+            stack -= per
+        # nb_job_soumis += 1
+        # print(t)
+    stack += dt
     cluster1.update(dt)
-    #cluster2.update(dt)
+    # list1.append(cluster1.get_len())
+    cluster2.update(dt)
+    # list2.append(cluster2.get_len())
     load1.append(100*cluster1.get_occup()/cluster1.total_proc)
-    #load2.append(100*cluster2.get_occup()/cluster2.total_proc)
-    i += 2
+    load2.append(100*cluster2.get_occup()/cluster2.total_proc)
     if np.isclose(t%1, 0) :
         print("SIMULATION TIME : %2.2f s" %(t))
 t2 = time.time()
@@ -89,16 +101,40 @@ ax1.set_ylim(-5,105); ax1.grid()
 ax2.plot(tn,cluster1.liste_theta_star,'b-')
 ax2.set(xlabel='Time (s)', ylabel='\u03B8* (sec)')
 ax2.grid()
+# ax3.plot(tn,list1,'g-')
+# ax3.set(xlabel='Time (s)', ylabel='Waiting queue')
+# ax3.grid()
 fig1.show()
 
-# fig2, (ax1, ax2) = plt.subplots(2, sharex=True)
-# fig2.suptitle('Cluster 2')
-# ax1.plot(tn,load2,'r-')
-# ax1.set(xlabel='Time (s)', ylabel='CPU Occupation (%)')
-# ax1.set_ylim(-5,105); ax1.grid()
-# ax2.plot(tn,cluster2.liste_theta_star,'b-')
-# ax2.set(xlabel='Time (s)', ylabel='\u03B8* (sec)')
-# ax2.grid()
-# fig2.show()
+fig2, (ax1, ax2) = plt.subplots(2, sharex=True)
+fig2.suptitle('Cluster 2')
+ax1.plot(tn,load2,'r-')
+ax1.set(xlabel='Time (s)', ylabel='CPU Occupation (%)')
+ax1.set_ylim(-5,105); ax1.grid()
+ax2.plot(tn,cluster2.liste_theta_star,'b-')
+ax2.set(xlabel='Time (s)', ylabel='\u03B8* (sec)')
+ax2.grid()
+# ax3.plot(tn,list2,'g-')
+# ax3.set(xlabel='Time (s)', ylabel='Waiting queue')
+# ax3.grid()
+fig2.show()
 
 print('Duration of the simulation : %.3f seconds'%(t2-t1))
+
+# dt = np.array([1e-1,5e-2,1e-2,5e-3,1e-3])
+# time = np.array([2.335,4.715,12.874,23.014,100.1])
+
+# X = np.array([np.ones(len(dt)),np.log(dt)]).T
+# y = np.log(time)
+
+# line = npl.solve(X.T.dot(X),X.T.dot(y))
+
+# plt.close('all')
+# plt.plot(np.log(dt),np.log(time),'r*-',label='Log of time simulation')
+# plt.plot(np.log(dt),-(3/4)*np.log(dt)+line[0],'--',label='o($dt^{-3/4}$)')
+# plt.title('Time of simulation, depending on the time step')
+# plt.xlabel('log(dt)')
+# plt.ylabel('log(t)')
+# plt.legend()
+# plt.grid()
+# plt.show()
